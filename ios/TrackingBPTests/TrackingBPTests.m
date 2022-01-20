9980664aa5e92f14c441c0c762d9d8326d8933d6
jcaviedesc <jcamilocc97@gmail.com>
2022-01-19 22:06:52 -0500

    rename package com.seguimientoHtaApp -> com.trackingbp

--- ios/TrackingBPTests/TrackingBPTests.m
+++ ios/TrackingBPTests/TrackingBPTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface SeguimientoHaAppTests : XCTestCase
+@interface TrackingBPTests : XCTestCase
 
 @end
 
-@implementation SeguimientoHaAppTests
+@implementation TrackingBPTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
