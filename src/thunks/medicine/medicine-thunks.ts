b0bd6a19e7684fd5502b691b2fb7286d0a73a08a
jcaviedesc <jcamilocc97@gmail.com>
2022-09-25 15:03:23 -0500

    add clean object

--- src/thunks/medicine/medicine-thunks.ts
+++ src/thunks/medicine/medicine-thunks.ts
@@ -1,5 +1,6 @@
 import crashlytics from '@react-native-firebase/crashlytics';
 import { createAsyncThunk } from '@reduxjs/toolkit';
+import { cleanObject } from '../../services/utils';
 import type { ClientApi, RootState } from '../../store/configureStore';
 
 export const fetchListMedicine = createAsyncThunk<
@@ -13,7 +14,7 @@ export const fetchListMedicine = createAsyncThunk<
   'consult-medicine-up/',
   async (_: void, { extra: clientApi, rejectWithValue }) => {
     const response = await clientApi.consultListMedicine();
-    
+
     if (response.status >= 300) {
       crashlytics().recordError(response.error);
       return rejectWithValue(response.data);
@@ -35,15 +36,12 @@ export const fetchAddMedicine = createAsyncThunk<
   'medicine-create',
   async (Medicine, { extra: clientApi, rejectWithValue }) => {
     // TODO select language
-   
 
     try {
-      const buildRequestData = Medicine;
+      const buildRequestData = cleanObject(Medicine);
       const response = await clientApi.addMedicine(buildRequestData);
-      console.log('response',response)
       return Promise.resolve(response.data);
     } catch (error) {
-      console.log('errror',error.message.detail)
       return rejectWithValue(error);
     }
   },
